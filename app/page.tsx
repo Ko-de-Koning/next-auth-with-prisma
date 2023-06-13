9a320849f72a505b17b69751a9f6fc4f1583b74b
Ko-de-Koning <110471206+Ko-de-Koning@users.noreply.github.com>
2023-06-13 16:26:11 +0200

    header_added

--- app/page.tsx
+++ app/page.tsx
@@ -11,13 +11,24 @@ export default async function Home() {
   const session = await getServerSession(authOptions);
 
   return (
-    <main>
-      <LoginButton />
-      <LogoutButton />
-      <h2>Server Session</h2>
-      <pre>{JSON.stringify(session)}</pre>
-      <h2>Client Call</h2>
-      <User />
-    </main>
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">Todos</h1>
+        <Link
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          href="/new"
+        >
+          New
+        </Link>
+      </header>
+      <main>
+        <LoginButton />
+        <LogoutButton />
+        <h2>Server Session</h2>
+        <pre>{JSON.stringify(session)}</pre>
+        <h2>Client Call</h2>
+        <User />
+      </main>
+    </>
   );
 }
