061be853872856979eb2c0f1dd7b89d0ab2a43b2
Ko-de-Koning <110471206+Ko-de-Koning@users.noreply.github.com>
2023-06-13 15:09:27 +0200

    tailwindcss added

--- app/auth.tsx
+++ app/auth.tsx
@@ -1,11 +1,25 @@
-'use client'
+"use client";
 
-import { signIn, signOut } from 'next-auth/react'
+import { signIn, signOut } from "next-auth/react";
 
 export const LoginButton = () => {
-  return <button onClick={() => signIn()}>Sign in</button>
-}
+  return (
+    <button
+      className="border border-teal-500 bg-teal-500 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-teal-600 focus:outline-none focus:shadow-outline"
+      onClick={() => signIn()}
+    >
+      Sign in
+    </button>
+  );
+};
 
 export const LogoutButton = () => {
-  return <button onClick={() => signOut()}>Sign Out</button>
-}
+  return (
+    <button
+      className="border border-teal-500 bg-teal-500 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-teal-600 focus:outline-none focus:shadow-outline"
+      onClick={() => signOut()}
+    >
+      Sign Out
+    </button>
+  );
+};
